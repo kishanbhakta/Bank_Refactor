49b4f266e208ac4d6fa4ad4f1cb1598a97333986
Kishan Bhakta <kishan.d.bhakta@gmail.com>
2016-02-10 15:16:29 -0500

    Changed alot

--- refactored.js
+++ refactored.js
@@ -9,60 +9,67 @@ var retirementWithdrawBtn = document.querySelector("#withdraw2");
 console.log(retirementWithdrawBtn);
 
 
+var checkingBalance = [];
+var balance1 = 0;
+var balance2 = 0;
+
 //Create a function for Checking Account and munipulate balance total from amount deposited
-  var checkingAcct = function () {
-    var balance1 = 0;
-    var amount = document.getElementById("amount1").val();
-        amount = parseInt(amount);
+var checkingAcct = function() {
+    var amount = document.getElementById("amount1").value;
+    amount = parseInt(amount);
 
     //Develope if...else statements to add amount value when deposited or substract amount value when withdrawing
     function depositCheckingAcct() {
-      if(amount >= balance1){
-        balance = amount + balance1;
-      }
-      else(amount <= balance1)
-        balance1 = balance1 - amount;
+        balance1 = amount + balance1;
+        checkingBalance.push("balance1");
+    console.log(balance1);
     }
-    console.log(amount);
+    console.log(checkingBalance);
+    function withdrawCheckingAcct() {
+      if(amount <= balance1) {
+        balance1 = balance1 - amount;
+        checkingBalance.push(balance1);
+    console.log(balance1);
+      }
+    console.log(checkingBalance);
+    }
     updateDisplay();
-  };
-    checkingDepositBtn.addEventListener('click', checkingAcct);
+};
+checkingDepositBtn.addEventListener('click', checkingAcct);
+checkingWithdrawBtn.addEventListener('click', checkingAcct);
 
 //Create a function for Retirement Account and munipulate balance total from amount deposited
-  var retirementAcct = function () {
-    var balance2 = 0;
-    var amount = document.getElementById("amount2").val();
-        amount = parseInt(amount);
+var retirementAcct = function() {
+    var amount = document.getElementById("amount2").value;
+    amount = parseInt(amount);
 
     //Develope if...else statements to add amount value plus complimentary $10 from the bank when deposited or substract amount value when withdrawing
     function depositRetirementAcct() {
-      if(amount >= balance2){
-        balance2 = amount + balance2 + 10;
-      }
-      else(amount <= balance2)
+        if (amount >= balance2) {
+            balance2 = amount + balance2 + 10;
+        } else(amount <= balance2)
         balance2 = balance2 - amount;
     }
     updateDisplay();
-  };
-    retirement.addEventListener('click', retirementAcct);
+};
+retirementDepositBtn.addEventListener('click', retirementAcct);
+retirementWithdrawBtn.addEventListener('click', retirementAcct);
 
-
-  function updateDisplay (balance) {
-    if(balance1 <= 0)
-      document.getElementById("balance1").addClass("zero");
+function updateDisplay(balance) {
+    if (balance1 <= 0)
+        document.getElementById("balance1").className = "zero";
     else
-      document.getElementById("balance1").removeClass("zero");
+        document.getElementById("balance1").className = "";
 
-    if(balance2 <= 0)
-      document.getElementById("balance2").addClass("zero");
+    if (balance2 <= 0)
+        document.getElementById("balance2").className ="zero";
     else
-      document.getElementById("balance2").removeClass("zero");
+        document.getElementById("balance2").className = "";
 
-    document.getElementById('balance1').value("$" + balance1);
-    document.getElementById('balance2').value("$" + balance2);
-    document.getElementById('amount1').value("");
-    document.getElementById('amount2').value("");
-
-  };
-
-
+    var bal1Element = document.getElementById("balance1");
+        bal1Element.innerHTML = "$" + checkingBalance;
+    var bal2Element = document.getElementById("balance2");
+        bal2Element.innerHTML = "$" + balance2;
+    var amnt1Element = document.getElementById('amount1').value;
+    var amnt2Element = document.getElementById('amount2').value;
+};
